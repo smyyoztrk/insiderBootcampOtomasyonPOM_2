103f631ca55a3a5acde8714c7d5098c37f3e4522
smyyoztrk <s.kaya@msn.com>
2024-08-20 10:30:46 +0300

    assert yorum satırları test dosyasından silindi

--- tests/test_homepage_and_careers_page.py
+++ tests/test_homepage_and_careers_page.py
@@ -18,10 +18,10 @@ class TestHomepageAndCareersPage(softest.TestCase):
         homepage.click_menu_item(homepage.COMPANY_MENU)
         homepage.go_to_careers()
         assert careerspage.is_careers_page_loaded(), "Careers home page is not loaded"
-        # assert careerspage.is_teams_block_displayed(), "Teams block page is not loaded"
         self.soft_assert(self.assertTrue, careerspage.is_teams_block_displayed(), "Teams block page is not loaded")
-        # assert careerspage.is_locations_block_displayed(), "Locations block is not loaded"
+        
         self.soft_assert(self.assertTrue, careerspage.is_locations_block_displayed(), "Locations block is not loaded")
-        # assert careerspage.is_insiderat_life_block_displayed(), "Insider At Life block is not loaded"
+        
         self.soft_assert(self.assertTrue, careerspage.is_insiderat_life_block_displayed(), "Insider At Life block is not loaded")
+        
         self.assert_all()
